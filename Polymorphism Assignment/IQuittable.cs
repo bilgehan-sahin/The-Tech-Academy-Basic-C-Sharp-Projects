54195e865caece991bab46ff030bdd31f0cc870f
bilgehan-sahin <bilgehan.sahin@my-aolcc.com>
2024-10-28 15:38:40 +0300

    Polymorphism Assignment
    Polymorphism Assignment

---
+++ Polymorphism Assignment/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment
+{
+    public interface IQuittable
+    {
+        // Define a method signature for Quit() that classes implementing this interface must define.
+        void Quit();
+    }
+}
---
+++ Polymorphism Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object and assign values to its properties.
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            // Use polymorphism to create an object of type IQuittable.
+            IQuittable quittableEmployee = employee;
+
+            // Call the Quit() method on the IQuittable object.
+            quittableEmployee.Quit();
+
+            // Keep the console window open until the user presses Enter.
+            Console.ReadLine();
+        }
+    }
+}
